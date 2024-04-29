e846089899f14b2076b05d2bc5ce77f59a10ce6f
assilbekov <askhat.assilbekov@gmail.com>
2024-04-29 18:14:06 -0300

    add abstract factory getFactory

--- pkg/factory.go
+++ pkg/factory.go
@@ -1,6 +1,18 @@
 package pkg
 
+import "errors"
+
 type Factory interface {
 	CreateComputer() Computer
 	CreateMonitor() Monitor
+}
+
+func GetFactory(factoryName string) (Factory, error) {
+	if factoryName == "HP" {
+		return &HPFactory{}, nil
+	}
+	if factoryName == "Asus" {
+		return &AsusFactory{}, nil
+	}
+	return nil, errors.New("invalid factory name")
 }
