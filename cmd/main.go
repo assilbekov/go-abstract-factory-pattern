d1e21746a2a0ece1bf8804c2c195e53f8cdeccaf
assilbekov <askhat.assilbekov@gmail.com>
2024-04-29 18:16:02 -0300

    print details

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import "abstract-factory/pkg"
+
+func main() {
+	asusFactory, err := pkg.GetFactory("Asus")
+	if err != nil {
+		panic(err)
+	}
+	asusComputer := asusFactory.CreateComputer()
+	asusMonitor := asusFactory.CreateMonitor()
+
+	hpFactory, err := pkg.GetFactory("HP")
+	if err != nil {
+		panic(err)
+	}
+	hpComputer := hpFactory.CreateComputer()
+	hpMonitor := hpFactory.CreateMonitor()
+
+	asusComputer.PrintDetails()
+	asusMonitor.PrintDetails()
+
+	hpComputer.PrintDetails()
+	hpMonitor.PrintDetails()
+}
