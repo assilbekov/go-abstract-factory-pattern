ee61f8719c4f73ee7002c8f3cacb1d0bcc21dd54
assilbekov <askhat.assilbekov@gmail.com>
2024-04-29 18:06:42 -0300

    define asus factory options

--- pkg/asus-factory.go
+++ pkg/asus-factory.go
@@ -3,9 +3,14 @@ package pkg
 type AsusFactory struct{}
 
 func (a *AsusFactory) CreateComputer() Computer {
-	return &AsusComputer{}
+	return &AsusComputer{
+		Memory: 16,
+		CPU:    3,
+	}
 }
 
 func (a *AsusFactory) CreateMonitor() Monitor {
-	return &AsusMonitor{}
+	return &AsusMonitor{
+		Size: 27,
+	}
 }
